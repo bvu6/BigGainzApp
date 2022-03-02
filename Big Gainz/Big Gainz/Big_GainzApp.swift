e591d97d48de05ab9985ebc74089ea4275549b4f
Jason Leong <j_leong4@u.pacific.edu>
2022-03-01 23:09:56 -0800

    Boiler Plate Code

---
+++ Big Gainz/Big Gainz/Big_GainzApp.swift
@@ -0,0 +1,17 @@
+//
+//  Big_GainzApp.swift
+//  Big Gainz
+//
+//  Created by Jason Leong on 3/1/22.
+//
+
+import SwiftUI
+
+@main
+struct Big_GainzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
