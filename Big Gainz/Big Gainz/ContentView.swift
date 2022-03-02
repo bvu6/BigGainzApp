7cefacb1003ee7d602bd4eba782061f816f36d18
Jason Leong <j_leong4@u.pacific.edu>
2022-03-02 00:46:15 -0800

    Created starting page with a button that leads to home page view.

--- Big Gainz/Big Gainz/ContentView.swift
+++ Big Gainz/Big Gainz/ContentView.swift
@@ -9,10 +9,41 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                
+                Text("Big Gainz")
+                    .font((.system(size: 70, weight: .semibold, design: .serif)))
+                    .foregroundColor(Color.orange)
+                    .position(x: 200, y: 200)
+            
+                
+                NavigationLink(destination: HomePageView()) {
+                    Text("Let's Get Started!")
+                    .frame(minWidth: 0, maxWidth: 250)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.black]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(30)
+                    .font(.title)
+                    .position(x: 200, y: 50)
+                }
+            }
+            .background(Color.gray)
+        }
+//        Text("Big Gainz")
+//            .font((.system(size: 50, weight: .semibold, design: .serif)))
+//            .position(x: 200, y:200)
+//
+//        NavigationLink(destination: HomePageView()) {
+//            Button(action: {
+//                print("")
+//            })
+//                Text("Start")
+//        }
     }
 }
+        
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ Big Gainz/Big Gainz/HomePageView.swift
@@ -0,0 +1,20 @@
+//
+//  HomePageView.swift
+//  Big Gainz
+//
+//  Created by Jason Leong on 3/2/22.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text("Home Page")
+    }
+}
+
+struct HomePageView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomePageView()
+    }
+}
