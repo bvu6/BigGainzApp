a366fdbdbc998240fd84056669834767aa93d603
Brian Che <b_che@u.pacific.edu>
2022-03-20 01:06:17 -0700

    Added button to switch view and login info detail page

--- Big Gainz/Big Gainz/LoginInfoView.swift
+++ Big Gainz/Big Gainz/LoginInfoView.swift
@@ -19,26 +19,38 @@ struct LoginInfoView: View {
                     .position(x: 200)
             
                 RoundedRectangle(cornerRadius: 20)
-                    .stroke(Color.black, lineWidth: 10)
-                    .position(x: 155, y: 150)
-                    .frame(width: 300, height: 300)
+                    .stroke(Color.black, lineWidth: 6)
+                    .position(x: 180, y: 150)
+                    .frame(width: 350, height: 350)
                 
                 RoundedRectangle(cornerRadius: 20)
-                    .frame(width: 300, height: 300)
+                    .frame(width: 350, height: 350)
                     .foregroundColor(Color.gray)
-                    .position(x: 200, y: -158)
+                    .position(x: 200, y: 400)
                     .overlay(
                      Group { //<-- Here
-                        Text("Name:")
-                             .position(x: 100, y: -250)
-                        Text("Age:")
-                             .position(x: 92, y: -200)
-                        Text("Height:")
-                             .position(x: 100, y: -150)
-                        Text("Weight:")
-                             .position(x: 100, y: -100)
+                        Text("**Name:**")
+                             .position(x: 90, y: -340)
+                        Text("**Age:**")
+                             .position(x: 82, y: -280)
+                        Text("**Height:**")
+                             .position(x: 93, y: -220)
+                        Text("**Weight:**")
+                             .position(x: 93, y: -160)
                      }
                      )
+                
+                
+                NavigationLink(destination: MainLog()) {
+                    Text("Enter")
+                    .frame(minWidth: 0, maxWidth: 100)
+                    .padding()
+                    .foregroundColor(.white)
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.orange, Color.black]), startPoint: .leading, endPoint: .trailing))
+                    .cornerRadius(30)
+                    .font(.title)
+                    .position(x: 200, y: -155)
+                }
             }
             .background(Color.gray)
         }
